5de4fde8f4b65ab6a9fd31181a42affc26c8e249
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-01 18:23:46 +0200

    create new file dice page content

---
+++ lib/app/dice_page/dice_page_content.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DicePageContent extends StatelessWidget {
+  const DicePageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
