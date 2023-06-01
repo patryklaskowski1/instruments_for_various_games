d90de5d085a482dad8578d49883a660a38197dea
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-01 19:40:22 +0200

    implement random event to on press elevatedButton

--- lib/app/dice_page/dice_page_content.dart
+++ lib/app/dice_page/dice_page_content.dart
@@ -1,10 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DicePageContent extends StatelessWidget {
+class DicePageContent extends StatefulWidget {
   const DicePageContent({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<DicePageContent> createState() => _DicePageContentState();
+}
+
+class _DicePageContentState extends State<DicePageContent> {
+  int number = 1;
+  Random randomNumber = Random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +38,34 @@ class DicePageContent extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            InkWell(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50.0),
-                child: Image.asset('images/dice1.png',
-                    width: 180.0, height: 180.0),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(80.0),
+                    child: Image.asset('images/dice$number.png',
+                        width: 180.0, height: 180.0),
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 70),
             ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'ROLL',
-              ),
-            ),
+                style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 33, 99, 35),
+                    fixedSize: const Size(180, 80),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50))),
+                child: const Text(
+                  'ROLL',
+                  style: TextStyle(fontSize: 24),
+                ),
+                onPressed: () {
+                  setState(() {
+                    number = randomNumber.nextInt(6) + 1;
+                  });
+                }),
           ],
         ),
       ),
