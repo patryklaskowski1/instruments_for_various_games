1d14bb3049a0803bf07b5cbe2db5b2ea191b3cdd
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-02 19:07:55 +0200

    change title main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Instruments for various games',
+      title: 'Tools for various games',
       home: HomePage(),
     );
   }
