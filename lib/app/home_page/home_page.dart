b7cf4a477f0940642b71ae31a989dc2c8e560b35
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-06 18:47:49 +0200

    Change fontSize Text for invidual ResponsivLayout

--- lib/app/home_page/home_page.dart
+++ lib/app/home_page/home_page.dart
@@ -30,7 +30,7 @@ class _UltraWideHomePage extends StatelessWidget {
         title: const Text(
           'What you need ?',
           style: TextStyle(
-            fontSize: 36,
+            fontSize: 38,
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 33, 99, 35),
@@ -158,7 +158,7 @@ class _WideHomePage extends StatelessWidget {
         title: const Text(
           'What you need ?',
           style: TextStyle(
-            fontSize: 42,
+            fontSize: 32,
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 33, 99, 35),
